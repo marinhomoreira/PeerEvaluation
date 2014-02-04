36e2c0ef21cfff2d221b29301b809eb511db085e
Marinho Moreira <fmarinho.rodrigues@gmail.com>
2014-02-04 15:33:59 -0700

    Improved UI. User can read instructions. User can fill evaluation for up to 8 team memebers.

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ var evaluationProvider = new EvaluationProvider('localhost', 27017);
 //TODO: Fake LOGIN
 app.get('/', routes.index);
 
+	
 // list all evaluations
 app.get('/evaluation/all', function(req, res){
 	evaluationProvider.findAll(function(error, evals){
@@ -55,9 +56,44 @@ app.get('/evaluation/new', function(req, res) {
 //save new evaluation
 app.post('/evaluation/new', function(req, res){
 	evaluationProvider.save({
+			iteration:0,
+	
 			ucid:req.param('ucid'),
-			grade:req.param('memberGrade'),
-			comment:req.param('comment'),
+			evaluator:req.param('evaluator'),
+			group:req.param('groupNumber'),
+			
+			teamMember1:req.param('teamMember1'),
+			grade1:req.param('memberGrade1'),
+			comment1:req.param('comment1'),
+
+			teamMember2:req.param('teamMember2'),
+			grade2:req.param('memberGrade2'),
+			comment2:req.param('comment2'),
+
+			teamMember3:req.param('teamMember3'),
+			grade3:req.param('memberGrade3'),
+			comment3:req.param('comment3'),
+
+			teamMember4:req.param('teamMember4'),
+			grade4:req.param('memberGrade4'),
+			comment4:req.param('comment4'),
+			
+			teamMember5:req.param('teamMember5'),
+			grade5:req.param('memberGrade5'),
+			comment5:req.param('comment5'),
+			
+			teamMember6:req.param('teamMember6'),
+			grade6:req.param('memberGrade6'),
+			comment6:req.param('comment6'),
+
+			teamMember7:req.param('teamMember7'),
+			grade7:req.param('memberGrade7'),
+			comment7:req.param('comment7'),
+			
+			teamMember8:req.param('teamMember8'),
+			grade8:req.param('memberGrade8'),
+			comment8:req.param('comment8'),
+			
 	}, function( error, docs) {
 		res.redirect('/')
 	});
