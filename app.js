9cce70c93b882238e0c3764109406a2ea87da2cf
Marinho Moreira <fmarinho.rodrigues@gmail.com>
2014-02-12 16:33:23 -0700

    Requests logger method implemented.

--- app.js
+++ app.js
@@ -171,6 +171,7 @@ app.post('/evaluation/new', function(req, res){
 
 // list all students
 app.get('/student/all', function(req, res){
+	logRequest(req);
 	studentProvider.findAll(function(error, svals){
 		res.render('student_all', {
 			title: 'Students',
@@ -180,6 +181,9 @@ app.get('/student/all', function(req, res){
 });
 
 app.get('/student/new', function(req, res) {
+	logRequest(req);
+	//console.log(res);
+	// TODO: REMOVE THIS!
     studentProvider.save({
 			ucid:"123456",
 			name:"John Smith",
@@ -199,6 +203,34 @@ app.get('/student/new', function(req, res) {
 		});
 	
 });
+
+
+
+
+Date.prototype.format = function(format) //author: meizz
+{
+  var o = {
+    "M+" : this.getMonth()+1, //month
+    "d+" : this.getDate(),    //day
+    "h+" : this.getHours(),   //hour
+    "m+" : this.getMinutes(), //minute
+    "s+" : this.getSeconds(), //second
+    "q+" : Math.floor((this.getMonth()+3)/3),  //quarter
+    "S" : this.getMilliseconds() //millisecond
+  }
+
+  if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
+    (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+  for(var k in o)if(new RegExp("("+ k +")").test(format))
+    format = format.replace(RegExp.$1,
+      RegExp.$1.length==1 ? o[k] :
+        ("00"+ o[k]).substr((""+ o[k]).length));
+  return format;
+}
+
+function logRequest(req) {
+	console.log(new Date().format("yy-MM-dd h:mm:ss") +" "+ req.socket._peername.address + ":"+ req.socket._peername.port + " " + req.method + " " + req.url);
+}
 
 
 
