7bba6aa422433f292de9cdfc6bfe654570590b4a
Marinho Moreira <fmarinho.rodrigues@gmail.com>
2014-02-18 19:09:30 -0700

    Added form on main page for user validation. Created user.authenticate route. Created evaluation routes file.

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,12 @@
 
-/*
- * GET users listing.
- */
-
 exports.list = function(req, res){
-  res.send("respond with a resource");
+	res.send("respond with a resource");
 };
+
+exports.authenticate = function(req, res){
+	//TODO USER AUTHENTICATION
+	// Redirect to new evaluation form if info provided is from a valid student!
+	// What if is from an admin? :D
+	res.redirect('/evaluation/new/123456/supplier/1');
+	//res.redirect('/evaluation/new/'+req.params.ucid+"/"+req.params.type+"/"+req.params.group);
+}
